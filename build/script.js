cfe2c4f59fbcd8a4920315a3b309bf54340efa5f
Miguel Vilhena <mvmories@gmail.com>
2023-01-10 23:02:18 +0100

    Updated the bio, skills and links

--- build/script.js
+++ build/script.js
@@ -1,25 +1,26 @@
-
-$('.smooth-goto').on('click', function () {
-  $('html, body').animate({
-      scrollTop: $(this.hash).offset().top + 50
-  }, 1000);
+$(".smooth-goto").on("click", function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(this.hash).offset().top + 50,
+    },
+    1000
+  );
 });
 
-$(document).ready(function(){
-  $('.popup__img').css('visibility', 'visible');        
-})
-
+$(document).ready(function () {
+  $(".popup__img").css("visibility", "visible");
+});
 
 const miguel = {
-  age: 27,
-  location: 'lisboa',
+  age: 29,
+  location: "lisboa",
   certifications: {
-    branding: true,
     uxui: true,
     frontend: true,
     fullstack: true,
+    mobile: true,
   },
   make_eggs() {
-    console.log('hey');
-  }
-}
+    console.log("hey");
+  },
+};
